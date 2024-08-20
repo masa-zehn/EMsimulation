21f227d19e63eb4b0ceabc1e7f484bd28ee4017d
masa-zehn <shigi-masanori-01@g.ecc.u-tokyo.ac.jp>
2024-08-20 13:37:28 +0900

    append electric charge to particle.py

--- particle.py
+++ particle.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 class Particle:
-    mass = 0
+    mass = 0.0
     pos = np.ndarray((3,1))
+	charge = 0.0
 
     def mass_set(self,m):
         self.mass = m
     def pos_set(self,p):
         self.pos = p
+	def charge_set(self,c):
+		self.charge = c
 
 
 
